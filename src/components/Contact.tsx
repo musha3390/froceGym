d5452b893c015c0785e3c6cae7453129c6351e8e
musha3390 <mushahidr317@gmail.com>
2026-04-19 14:27:31 +0530

    updated the ifram url

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -219,7 +219,7 @@ export default function Contact() {
             {/* Google Map Placeholder - using an iframe */}
             <div className="rounded-lg overflow-hidden h-[250px]">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3430.407788461582!2d76.7118231!3d30.707011!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fee8ed8ab107d%3A0xc4f4dc8f06bb354c!2sSector%2058%2C%20Sahibzada%20Ajit%20Singh%20Nagar%2C%20Punjab!5e0!3m2!1sen!2sin!4v1701332402100!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d857.4925335518888!2d76.70630326968455!3d30.719239998411908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzDCsDQzJzA5LjMiTiA3NsKwNDInMjUuMCJF!5e0!3m2!1sen!2sin!4v1776588608605!5m2!1sen!2sin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
